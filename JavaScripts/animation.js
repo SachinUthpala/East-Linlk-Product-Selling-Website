128cada9ff504037e113edc8a5eaeb2547df0966
Sachin Gunasekara <gunasekara.s.u.d.a@gmail.com>
2024-06-18 11:18:05 +0530

    animation js

---
+++ JavaScripts/animation.js
@@ -0,0 +1,34 @@
+/*code for page animations */
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entrie) => {
+        console.log(entrie)
+        if(entrie.isIntersecting){
+            entrie.target.classList.add('showx');
+        }else{
+            entrie.target.classList.remove('showx');
+        }
+    });
+})
+
+const hiddenElementx = document.querySelectorAll('.hiddenx');
+hiddenElementx.forEach((el) =>observer.observe(el))
+
+/*end of the animation code */
+
+const observery = new IntersectionObserver((entries) => {
+    entries.forEach((entrie) => {
+        console.log(entrie)
+        if(entrie.isIntersecting){
+            entrie.target.classList.add('showy');
+        }else{
+            entrie.target.classList.remove('showy');
+        }
+    });
+})
+
+const hiddenElementy = document.querySelectorAll('.hiddeny');
+hiddenElementy.forEach((el) =>observery.observe(el))
+
+
+/*end of the animation code */
