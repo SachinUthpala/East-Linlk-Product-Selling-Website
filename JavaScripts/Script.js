742722636120511d87fe5a353059b7d9f49ae3d6
Sachin Gunasekara <gunasekara.s.u.d.a@gmail.com>
2024-06-24 09:21:34 +0530

    section03 completed

--- JavaScripts/Script.js
+++ JavaScripts/Script.js
@@ -31,3 +31,25 @@ function topFunction() {
       window.addEventListener("load" , function(){
         loader.style.display = "none";
       })
+
+
+
+      // for counter
+
+      const counts = document.querySelectorAll('.count')
+      const speed = 20
+
+      counts.forEach((counter) => {
+          function upDate(){
+              const target = Number(counter.getAttribute('data-target'))
+              const count = Number(counter.innerText)
+              const inc = target / speed        
+              if(count < target){
+                  counter.innerText = Math.floor(inc + count) 
+                  setTimeout(upDate, 15)
+              }else{
+                  counter.innerText = target
+              }
+          }
+          upDate()
+      })
