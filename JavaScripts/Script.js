a91c9b9fe2e6839c53f02d3e2ec604c1393a101e
Sachin Gunasekara <gunasekara.s.u.d.a@gmail.com>
2024-06-26 11:06:25 +0530

    125152

--- JavaScripts/Script.js
+++ JavaScripts/Script.js
@@ -95,7 +95,7 @@ function topFunction() {
 
 
 
-  const formOpenBtn = document.querySelector("#form-open") || document.querySelector("#form-open2"),
+  const formOpenBtn = document.querySelector("#form-open") ,
   home = document.querySelector(".home"),
   formContainer = document.querySelector(".form_container"),
   formCloseBtn = document.querySelector(".form_close"),
@@ -104,6 +104,35 @@ function topFunction() {
   pwShowHide = document.querySelectorAll(".pw_hide");
 
 formOpenBtn.addEventListener("click", () => home.classList.add("show"));
+formCloseBtn.addEventListener("click", () => home.classList.remove("show"));
+
+pwShowHide.forEach((icon) => {
+  icon.addEventListener("click", () => {
+    let getPwInput = icon.parentElement.querySelector("input");
+    if (getPwInput.type === "password") {
+      getPwInput.type = "text";
+      icon.classList.replace("uil-eye-slash", "uil-eye");
+    } else {
+      getPwInput.type = "password";
+      icon.classList.replace("uil-eye", "uil-eye-slash");
+    }
+  });
+});
+
+signupBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  formContainer.classList.add("active");
+});
+loginBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  formContainer.classList.remove("active");
+});
+
+// FORM OPEN 2
+
+const formOpenBtn2 = document.querySelector("#form-open2")
+
+formOpenBtn2.addEventListener("click", () => home.classList.add("show"));
 formCloseBtn.addEventListener("click", () => home.classList.remove("show"));
 
 pwShowHide.forEach((icon) => {
